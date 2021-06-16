e9db7dedccbd991b729ba3eea931a65e60b5fc64
wuchangwei <985215993@qq.com>
2021-06-16 16:18:20 +0800

    使用cheerio进行数据提取返回到一个result数组并且包含每次的时间戳

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,12 +1,34 @@
 import superagent from 'superagent' //ts引入js ts不会用
+import cheerio from 'cheerio' //就可以通过jQ语法去获取页面的内容
 //ts -> .d.ts 翻译文件 ->js
+interface Course {
+  title: string
+  count: number
+}
 class Crowller {
   private secret = 'x3b174jsx'
   private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`
-  private rawHtml = ''
+  getCourseInfo(html: string) {
+    //通过cheerio分析
+    const $ = cheerio.load(html)
+    const courseItems = $('.course-item')
+    const courseInfos: Course[] = []
+    courseItems.map((index, ele) => {
+      const descs = $(ele).find('.course-desc')
+      const title = descs.eq(0).text() //爬取每个课程的标题
+      const count = parseInt(descs.eq(1).text().split('：')[1]) //获取观看人数
+      courseInfos.push({ title, count })
+    })
+    const result = {
+      time: new Date().getTime(), //获取时间戳当前的
+      data: courseInfos,
+    }
+    console.log(result)
+  }
   async getRawHtml() {
     const result = await superagent.get(this.url)
-    this.rawHtml = result.text
+    //对rawHtml进行分析
+    this.getCourseInfo(result.text)
   }
   constructor() {
     this.getRawHtml()
