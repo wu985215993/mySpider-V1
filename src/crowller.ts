86c82476c21d470147d9215716da6702a6e9fe6b
wuchangwei <985215993@qq.com>
2021-06-16 17:34:39 +0800

    使用fs与path将每一次爬取到的内容输出到data/course.json中

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,10 +1,21 @@
 import superagent from 'superagent' //ts引入js ts不会用
 import cheerio from 'cheerio' //就可以通过jQ语法去获取页面的内容
+import fs from 'fs'
+import path from 'path'
 //ts -> .d.ts 翻译文件 ->js
 interface Course {
   title: string
   count: number
 }
+interface courseResult {
+  time: number
+  data: Course[]
+}
+//定义json的文件结构
+interface Content {
+  [propName: number]: Course[]
+}
+
 class Crowller {
   private secret = 'x3b174jsx'
   private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`
@@ -19,19 +30,33 @@ class Crowller {
       const count = parseInt(descs.eq(1).text().split('：')[1]) //获取观看人数
       courseInfos.push({ title, count })
     })
-    const result = {
+    return {
       time: new Date().getTime(), //获取时间戳当前的
       data: courseInfos,
     }
-    console.log(result)
   }
   async getRawHtml() {
     const result = await superagent.get(this.url)
     //对rawHtml进行分析
-    this.getCourseInfo(result.text)
+    return result.text
+  }
+  generateJsonContent(courseInfo: courseResult) {
+    const filePath = path.resolve(__dirname, '../data/course.json')
+    let fileContent: Content = {}
+    if (fs.existsSync(filePath)) {
+      //判断对应路径的文件是否存在
+      fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+    }
+    fileContent[courseInfo.time] = courseInfo.data //把新爬到的内容存到fileContent
+    fs.writeFileSync(filePath, JSON.stringify(fileContent))
+  }
+  async initSpiderProcess() {
+    const html = await this.getRawHtml()
+    const courseInfo = this.getCourseInfo(html)
+    this.generateJsonContent(courseInfo)
   }
   constructor() {
-    this.getRawHtml()
+    this.initSpiderProcess()
   }
 }
 
