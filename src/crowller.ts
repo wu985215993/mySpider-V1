f7142341dd642d9f82f8024db0f6a14a1d371aa2
wuchangwei <985215993@qq.com>
2021-06-21 19:31:42 +0800

    将爬取方式拆分到不同的文件

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,66 +1,32 @@
 import superagent from 'superagent' //ts引入js ts不会用
-import cheerio from 'cheerio' //就可以通过jQ语法去获取页面的内容
 import fs from 'fs'
 import path from 'path'
 //ts -> .d.ts 翻译文件 ->js
-interface Course {
-  title: string
-  count: number
-}
-interface courseResult {
-  time: number
-  data: Course[]
-}
-//定义json的文件结构
-interface Content {
-  [propName: number]: Course[]
-}
+import DellAnalyzer from './dellAnalyzer'
 
 class Crowller {
   private secret = 'x3b174jsx'
   private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`
-  getCourseInfo(html: string) {
-    //通过cheerio分析
-    const $ = cheerio.load(html)
-    const courseItems = $('.course-item')
-    const courseInfos: Course[] = []
-    courseItems.map((index, ele) => {
-      const descs = $(ele).find('.course-desc')
-      const title = descs.eq(0).text() //爬取每个课程的标题
-      const count = parseInt(descs.eq(1).text().split('：')[1]) //获取观看人数
-      courseInfos.push({ title, count })
-    })
-    return {
-      time: new Date().getTime(), //获取时间戳当前的
-      data: courseInfos,
-    }
-  }
+  private filePath = path.resolve(__dirname, '../data/course.json')
+  //获取html字符串
   async getRawHtml() {
     const result = await superagent.get(this.url)
     //对rawHtml进行分析
     return result.text
   }
-  generateJsonContent(courseInfo: courseResult) {
-    //得到course.json的文件路径
-    const filePath = path.resolve(__dirname, '../data/course.json')
-    let fileContent: Content = {}
-    if (fs.existsSync(filePath)) {
-      //判断对应路径的文件是否存在
-      fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
-    }
-    fileContent[courseInfo.time] = courseInfo.data
-    return fileContent //把新爬到的内容存到fileContent
+  //写文件的函数
+  writeFile(content: string) {
+    fs.writeFileSync(this.filePath, content)
   }
+  //初始化爬虫的进程
   async initSpiderProcess() {
     const html = await this.getRawHtml()
-    const courseInfo = this.getCourseInfo(html)
-    const fileContent = this.generateJsonContent(courseInfo)
-    const filePath = path.resolve(__dirname, '../data/course.json')
-    fs.writeFileSync(filePath, JSON.stringify(fileContent))
+    const fileContent = this.analyzer.analyze(html, this.filePath)
+    this.writeFile(fileContent)
   }
-  constructor() {
+  constructor(private analyzer: any) {
     this.initSpiderProcess()
   }
 }
-
-const crowller = new Crowller()
+const analyzer = new DellAnalyzer()
+const crowller = new Crowller(analyzer)
---
+++ src/dellAnalyzer.ts
@@ -0,0 +1,51 @@
+import cheerio from 'cheerio' //就可以通过jQ语法去获取页面的内容
+import fs from 'fs'
+
+interface Course {
+  title: string
+  count: number
+}
+interface courseResult {
+  time: number
+  data: Course[]
+} //定义json的文件结构
+interface Content {
+  [propName: number]: Course[]
+}
+export default class DellAnalyzer {
+  //通过获取到html字符串获取需要爬取的数据并返回一个对象 对象接口类型是courseResult
+  private getCourseInfo(html: string) {
+    //通过cheerio分析
+    const $ = cheerio.load(html)
+    const courseItems = $('.course-item')
+    const courseInfos: Course[] = []
+    courseItems.map((index, ele) => {
+      const descs = $(ele).find('.course-desc')
+      const title = descs.eq(0).text() //爬取每个课程的标题
+      const count = parseInt(descs.eq(1).text().split('：')[1]) //获取观看人数
+      courseInfos.push({ title, count })
+    })
+    return {
+      time: new Date().getTime(), //获取时间戳当前的
+      data: courseInfos,
+    }
+  }
+  //将获取到的 courseResult类型的数据存放到磁盘
+  generateJsonContent(courseInfo: courseResult, filePath: string) {
+    //得到course.json的文件路径
+    let fileContent: Content = {}
+    if (fs.existsSync(filePath)) {
+      //判断对应路径的文件是否存在
+      fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+    }
+    fileContent[courseInfo.time] = courseInfo.data
+    return fileContent //把新爬到的内容存到fileContent
+  }
+
+  public analyze(html: string, filePath: string) {
+    //将分析的过程移动到这里做
+    const courseInfo = this.getCourseInfo(html)
+    const fileContent = this.generateJsonContent(courseInfo, filePath) //生成要存储的内容
+    return JSON.stringify(fileContent)
+  }
+}
